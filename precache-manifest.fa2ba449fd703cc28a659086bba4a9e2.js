e6817c2964082080c38c73b11b454b63eb901358
fazli596 <abdullahfazli99@gmail.com>
2019-10-29 01:01:58 +0530

    Updates

---
+++ precache-manifest.fa2ba449fd703cc28a659086bba4a9e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39a453d8b9a7530547dc92d707dbec5b",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "ee93588e1a8cdecdc533",
+    "url": "/contactmanager/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "59761e87ffa43334e9e5",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "ee93588e1a8cdecdc533",
+    "url": "/contactmanager/static/js/2.4684605c.chunk.js"
+  },
+  {
+    "revision": "59761e87ffa43334e9e5",
+    "url": "/contactmanager/static/js/main.7dd09a27.chunk.js"
+  },
+  {
+    "revision": "110baf0faa163ffdda46",
+    "url": "/contactmanager/static/js/runtime-main.7792f807.js"
+  }
+]);
